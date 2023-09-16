e632919f6db09964c6a0c5443dc508e5eb378f14
amol009jobsearch <67626557+amol009jobsearch@users.noreply.github.com>
2023-09-16 06:33:03 +0530

    basic dataType of str, int, float, decimal and TypeCasting

--- src/main/basicDatatype/basicDataTypePractice.py
+++ src/main/basicDatatype/basicDataTypePractice.py
@@ -1,1 +1,38 @@
+from decimal import Decimal
+
 print("Hello python")
+
+intVar=3
+floatVar=3.0
+stringVar='India'
+decimalVar=Decimal('1.0')
+
+'''check the data types of the variables'''
+print(type(intVar))
+print(type(floatVar))
+print(type(stringVar))
+print(type(decimalVar))
+
+print('============')
+'''print the values of variable'''
+print(intVar)
+print(floatVar)
+print(stringVar)
+print(decimalVar)
+
+print('============')
+
+''' Type casting '''
+'''Float to Int conversion'''
+objFloat=2.5
+convertToInt=int(objFloat)
+print(convertToInt)
+
+objInt=2
+convertToFloat=float(objInt)
+print(convertToFloat)
+
+objInt=5
+convertToString=str(objInt)
+print(convertToString)
+print(type(convertToString))
